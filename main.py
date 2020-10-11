e508521e2850b5925e767444cb40f5a4caf9c94c
Samuel <berriens@gmail.com>
2020-10-11 04:21:46 +0200

    mlflow ok

--- main.py
+++ main.py
@@ -19,13 +19,33 @@ import argparse
 
 def main() -> None:
     parser = argparse.ArgumentParser("MusicGAN")
-    parser.add_argument("experiment", type=str, metavar="EXP_NAME")
-    parser.add_argument("out", type=str, metavar="OUT_PATH")
+
+    parser.add_argument(
+        "experiment",
+        type=str, metavar="EXP_NAME")
+
+    parser.add_argument(
+        "-o", "--out-path",
+        dest="out_path", type=str,
+        required=True)
+
+    parser.add_argument(
+        "-i", "--input-musics",
+        dest="input_musics",
+        required=True,
+        type=str)
 
     args = parser.parse_args()
 
+    if mlflow.get_experiment_by_name(args.experiment) is None:
+        mlflow.create_experiment(args.experiment)
+    mlflow.set_experiment(args.experiment)
+
     wavs_path = glob.glob(
-        "/home/samuel/Documents/MusicGAN/res/piano/*.flac")
+        args.input_musics)
+
+    mlflow.start_run(run_name="main")
+    mlflow.log_param("input_musics", wavs_path)
 
     data = read_audio.to_tensor(wavs_path, utils.N_FFT, utils.N_SEC)
 
@@ -33,15 +53,23 @@ def main() -> None:
     hidden_w = 25
     hidden_h = hidden_w
 
+    mlflow.log_param("hidden_channel", hidden_channel)
+    mlflow.log_param("hidden_w", hidden_w)
+    mlflow.log_param("hidden_h", hidden_h)
+
     gen = networks.Generator(hidden_channel)
     disc = networks.Discriminator(2)
     disc.cuda()
     gen.cuda()
 
-    nb_epoch = 100
+    nb_epoch = 5
     batch_size = 8
 
     nb_backward_gen = 3
+
+    mlflow.log_param("nb_epoch", nb_epoch)
+    mlflow.log_param("batch_size", batch_size)
+    mlflow.log_param("nb_backward_gen", nb_backward_gen)
 
     for i in tqdm(range(data.size(0) - 1)):
         j = i + random.randint(0, sys.maxsize) // (
@@ -61,88 +89,122 @@ def main() -> None:
         mean_d,
         cov_m)
 
+    mlflow.log_param("mean_v", mean_d.numpy().tolist())
+    mlflow.log_param("cov_m", cov_m.numpy().tolist())
+
     def _gen_rand(curr_batch_size: int) -> th.Tensor:
         cpx_vec = hidden_dist.sample(
             (curr_batch_size, hidden_w, hidden_h)).cuda()
 
         return cpx_vec.permute(0, 3, 1, 2)
 
-    for e in range(nb_epoch):
-        disc_loss_sum = 0.
-        gen_loss_sum = 0.
+    with mlflow.start_run(nested=True, run_name="train"):
+        for e in range(nb_epoch):
+            disc_loss_sum = 0.
+            gen_loss_sum = 0.
 
-        tqdm_bar = tqdm(range(nb_batch))
+            tqdm_bar = tqdm(range(nb_batch))
 
-        nb_tp = 0
-        nb_tn = 0
+            nb_tp = 0
+            nb_tn = 0
 
-        for b_idx in tqdm_bar:
-            i_min = b_idx * batch_size
-            i_max = (b_idx + 1) * batch_size
-            i_max = min(data.size(0), i_max)
+            for b_idx in tqdm_bar:
+                i_min = b_idx * batch_size
+                i_max = (b_idx + 1) * batch_size
+                i_max = min(data.size(0), i_max)
 
-            x_real = data[i_min:i_max, :, :, :].cuda()
+                x_real = data[i_min:i_max, :, :, :].cuda()
 
-            # Train discriminator
-            gen.eval()
-            disc.train()
+                # Train discriminator
+                gen.eval()
+                disc.train()
 
-            h_fake = _gen_rand(i_max - i_min)
-
-            x_fake = gen(h_fake)
-            out_real = disc(x_real)
-            out_fake = disc(x_fake)
-
-            nb_tp += (out_real > 0.5).sum().item()
-            nb_tn += (out_fake < 0.5).sum().item()
-
-            disc_loss = networks.discriminator_loss(out_real, out_fake)
-
-            gen_optimizer.zero_grad()
-            disc_optimizer.zero_grad()
-
-            disc_loss.backward()
-            disc_optimizer.step()
-
-            disc_loss_sum += disc_loss.item()
-
-            # Train generator
-            gen.train()
-            disc.eval()
-
-            for _ in range(nb_backward_gen):
                 h_fake = _gen_rand(i_max - i_min)
 
                 x_fake = gen(h_fake)
+                out_real = disc(x_real)
                 out_fake = disc(x_fake)
 
-                gen_loss = networks.generator_loss(out_fake)
+                nb_tp += (out_real > 0.5).sum().item()
+                nb_tn += (out_fake < 0.5).sum().item()
 
-                disc_optimizer.zero_grad()
+                disc_loss = networks.discriminator_loss(out_real, out_fake)
+
                 gen_optimizer.zero_grad()
+                disc_optimizer.zero_grad()
 
-                gen_loss.backward()
-                gen_optimizer.step()
+                disc_loss.backward()
+                disc_optimizer.step()
 
-                gen_loss_sum += gen_loss.item()
+                disc_loss_sum += disc_loss.item()
 
-            tqdm_bar.set_description(
-                f"Epoch {e} : disc_loss = {disc_loss_sum / (b_idx + 1):.6f}, "
-                f"gen_loss = {gen_loss_sum / ((b_idx + 1) * nb_backward_gen):.6f}, "
-                f"tp = {nb_tp / ((b_idx + 1) * batch_size):.4f}, "
-                f"tn = {nb_tn / ((b_idx + 1) * batch_size):.4f}")
+                # Train generator
+                gen.train()
+                disc.eval()
 
-        read_audio.to_wav(
-            gen(hidden_dist.sample(
-                (1, 10 * hidden_w, hidden_h))
-                .permute(0, 3, 1, 2).cuda()).detach().cpu(),
-            f"./out_2/out_train_epoch_{e}.wav")
+                batch_gen_loss = 0
+                for _ in range(nb_backward_gen):
+                    h_fake = _gen_rand(i_max - i_min)
 
-        th.save(disc.state_dict(), "./out_2/disc_1.pt")
-        th.save(gen.state_dict(), "./out_2/gen_1.pt")
+                    x_fake = gen(h_fake)
+                    out_fake = disc(x_fake)
 
-        th.save(disc_optimizer.state_dict(), "./out_2/disc_optim_1.pt")
-        th.save(gen_optimizer.state_dict(), "./out_2/gen_optim_1.pt")
+                    gen_loss = networks.generator_loss(out_fake)
+
+                    disc_optimizer.zero_grad()
+                    gen_optimizer.zero_grad()
+
+                    gen_loss.backward()
+                    gen_optimizer.step()
+
+                    gen_loss_sum += gen_loss.item()
+                    batch_gen_loss += gen_loss.item()
+
+                tqdm_bar.set_description(
+                    f"Epoch {e} : disc_loss = {disc_loss_sum / (b_idx + 1):.6f}, "
+                    f"gen_loss = {gen_loss_sum / ((b_idx + 1) * nb_backward_gen):.6f}, "
+                    f"tp = {nb_tp / ((b_idx + 1) * batch_size):.4f}, "
+                    f"tn = {nb_tn / ((b_idx + 1) * batch_size):.4f}")
+
+                mlflow.log_metric(
+                    "disc_loss", disc_loss.item(),
+                    step=e * nb_batch + b_idx)
+                mlflow.log_metric(
+                    "gen_loss", batch_gen_loss / nb_backward_gen,
+                    step=e * nb_batch + b_idx)
+                mlflow.log_metric(
+                    "batch_true_positive",
+                    (out_real > 0.5).to(th.float).mean().item(),
+                    step=e * nb_batch + b_idx)
+                mlflow.log_metric(
+                    "batch_true_negative",
+                    (out_fake < 0.5).to(th.float).mean().item(),
+                    step=e * nb_batch + b_idx)
+
+            _ = read_audio.to_wav(
+                gen(hidden_dist.sample(
+                    (1, 10 * hidden_w, hidden_h))
+                    .permute(0, 3, 1, 2).cuda()).detach().cpu(),
+                f"./out/out_train_epoch_{e}.wav")
+
+            with mlflow.start_run(nested=True, run_name=f"epoch_{e}"):
+                mlflow.log_artifact(f"./out/out_train_epoch_{e}.wav")
+                mlflow.pytorch.log_model(disc, "disc_model")
+                mlflow.pytorch.log_model(gen, "gen_model")
+
+                mlflow.log_metric(
+                    "disc_loss_mean",
+                    disc_loss_sum / nb_batch)
+                mlflow.log_metric(
+                    "gen_loss_mean",
+                    gen_loss_sum / nb_batch)
+                mlflow.log_metric(
+                    "true_positive_rate",
+                    nb_tp / (nb_batch * batch_size))
+                mlflow.log_metric(
+                    "true_negative_rate",
+                    nb_tn / (nb_batch * batch_size))
+    mlflow.end_run()
 
 
 if __name__ == '__main__':
