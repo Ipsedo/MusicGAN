a33e5fd0bd92072dc16bd5ff6a5382fd6d2a9488
Samuel <berriens@gmail.com>
2020-12-24 17:28:38 +0100

    test ae

--- main_adversarial_ae.py
+++ main_adversarial_ae.py
@@ -82,9 +82,9 @@ def main() -> None:
 
     nb_batch = math.floor(data.size(0) / batch_size)
 
-    disc_lr = 3e-4
-    gen_lr = 4e-4
-    enc_lr = 2e-4
+    disc_lr = 3e-5
+    gen_lr = 6e-5
+    enc_lr = 2e-5
 
     disc_optimizer = th.optim.Adagrad(disc.parameters(), lr=disc_lr)
     gen_optimizer = th.optim.Adagrad(gen.parameters(), lr=gen_lr)
