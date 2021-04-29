971a8871498c4b1d398e9b8b97647eac8680e3e9
Ipsedo <berriens@gmail.com>
2021-04-29 16:44:42 +0200

    wasserstein loss

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,5 +1,6 @@
-#from .discriminators import Discriminator, Discriminator1D, Discriminator3
-#from .gan_networks import Generator2, Generator, GeneratorBis
+# from .discriminators import Discriminator, Discriminator1D, Discriminator3
+# from .gan_networks import Generator2, Generator, GeneratorBis
 from .ticks_networks import Generator, Discriminator
-from .criterion import generator_loss, discriminator_loss
+from .criterion import generator_loss, discriminator_loss, \
+    wasserstein_generator_loss, wasserstein_discriminator_loss
 from .stft_networks import STFTGenerator, STFTDiscriminator
