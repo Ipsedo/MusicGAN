72215282487bcd140580ff0279b1c9d74c9ce025
Ipsedo <berriens@gmail.com>
2021-04-29 16:44:25 +0200

    use torchaudio

--- read_audio.py
+++ read_audio.py
@@ -5,6 +5,9 @@ import scipy.signal
 import soundfile as sf
 import pywt
 import torch as th
+import torch.nn.functional as th_f
+import torchaudio as th_audio
+import torchaudio.functional as th_audio_f
 import torch.fft as th_fft
 import numpy as np
 
@@ -223,6 +226,18 @@ def ticks_to_wav(data: th.Tensor, wav_path: str, sample_rate: int) -> None:
 ##########
 # STFT
 ##########
+def diff(x):
+    return th_f.pad(x[1:, :] - x[:-1, :], (0, 0, 1, 0), "constant", 0)
+
+
+def unwrap(phi):
+    dphi = diff(phi)
+    dphi_m = ((dphi + np.pi) % (2 * np.pi)) - np.pi
+    dphi_m[(dphi_m == -np.pi) & (dphi > 0)] = np.pi
+    phi_adj = dphi_m - dphi
+    phi_adj[dphi.abs() < np.pi] = 0
+    return phi + phi_adj.cumsum(0)
+
 
 def to_tensor_stft(
         wav_paths: List[str],
@@ -231,7 +246,6 @@ def to_tensor_stft(
 ) -> th.Tensor:
     nperseg = 1022
     stride = 256
-    noverlap = nperseg - stride
 
     nb_vec = 256
 
@@ -241,13 +255,13 @@ def to_tensor_stft(
 
     for wav_p in tqdm(wav_paths):
         # sample_rate, raw_audio = scipy.io.wavfile.read(wav_p)
-        raw_audio, curr_sample_rate = sf.read(wav_p)
+        raw_audio, curr_sample_rate = th_audio.load(wav_p)
 
         assert sample_rate == curr_sample_rate, \
             f"Needed = {sample_rate}Hz, " \
             f"actual = {curr_sample_rate}Hz"
 
-        nb_seg = raw_audio.shape[0] // stride
+        nb_seg = raw_audio.size()[1] // stride
 
         nb_batch += nb_seg // nb_vec
 
@@ -255,33 +269,34 @@ def to_tensor_stft(
 
     curr_batch = 0
 
+    hann_window = th.hann_window(nperseg)
+
     for wav_p in tqdm(wav_paths):
-        raw_audio, _ = sf.read(wav_p)
+        raw_audio, _ = th_audio.load(wav_p)
 
-        _, _, z = scipy.signal.stft(
-            raw_audio.mean(-1), nperseg=nperseg, noverlap=noverlap
+        raw_audio_mono = raw_audio.mean(0)
+
+        complex_values = th_audio_f.spectrogram(
+            raw_audio_mono,
+            pad=0, window=hann_window,
+            n_fft=nperseg, hop_length=stride, win_length=nperseg,
+            power=None, normalized=True
         )
 
-        z = z.transpose()
+        complex_values = complex_values.permute(1, 0, 2)
 
-        z = z[:z.shape[0] - z.shape[0] % nb_vec, :]
+        magn, phase = th_audio_f.magphase(complex_values)
 
-        # splitted_z = np.stack(
-        #    np.split(z, z.shape[0] // nb_vec, axis=0),
-        #    axis=0
-        # )
+        magn = th.log(magn + 1)
 
-        magn = np.absolute(z)
-        phase = np.unwrap(np.angle(z), axis=0)
+        phase = unwrap(phase)
+        phase = phase[1:, :] - phase[:-1, :]
+        magn = magn[1:, :]
 
-        phase[1:, :] = phase[1:, :] - phase[:-1, :]
-
-        magn = np.stack(
-            np.split(magn, magn.shape[0] // nb_vec, axis=0), axis=0
-        )
-        phase = np.stack(
-            np.split(phase, phase.shape[0] // nb_vec, axis=0), axis=0
-        )
+        magn = magn[magn.size()[0] % nb_vec:, :]
+        phase = phase[phase.size()[0] % nb_vec:, :]
+        magn = th.stack(magn.split(nb_vec, dim=0), dim=0)
+        phase = th.stack(phase.split(nb_vec, dim=0), dim=0)
 
         max_magn = magn.max()
         min_magn = magn.min()
@@ -291,13 +306,13 @@ def to_tensor_stft(
         magn = (magn - min_magn) / (max_magn - min_magn)
         phase = (phase - min_phase) / (max_phase - min_phase)
 
-        data[curr_batch:curr_batch + magn.shape[0], 0, :, :] = \
-            th.from_numpy(magn) * 2 - 1
+        data[curr_batch:curr_batch + magn.size()[0], 0, :, :] = \
+            magn * 2. - 1.
 
-        data[curr_batch:curr_batch + phase.shape[0], 1, :, :] = \
-            th.from_numpy(phase) * 2 - 1
+        data[curr_batch:curr_batch + phase.size()[0], 1, :, :] = \
+            phase * 2. - 1.
 
-        curr_batch += magn.shape[0]
+        curr_batch += phase.size()[0]
 
     return data
 
@@ -305,18 +320,21 @@ def to_tensor_stft(
 def stft_to_wav(x: th.Tensor, wav_path: str, sample_rate: int):
     x = x.permute(0, 2, 3, 1)
     x = x.flatten(0, 1)
-    x = x.numpy()
 
     phases = x[:, :, 1]
     for i in range(phases.shape[0] - 1):
         phases[i + 1, :] = phases[i + 1, :] + phases[i, :]
 
-    phases = phases * np.pi
+    phases = phases * np.pi % (2 * np.pi)
 
     magn = (x[:, :, 0] + 1) / 2
+    magn = th.exp(magn) - 1
 
-    real = magn * np.cos(phases)
-    imag = magn * np.sin(phases)
+    real = magn * th.cos(phases)
+    imag = magn * th.sin(phases)
+
+    real = real.numpy()
+    imag = imag.numpy()
 
     x = real + imag * 1j
     _, raw_audio = scipy.signal.istft(x.transpose(), nperseg=1022,
@@ -375,7 +393,7 @@ if __name__ == '__main__':
 
     stft_to_wav(out, "out.wav", 16000)
 
-    idx = 30
+    idx = 40
 
     real, imag = out[idx, 0, :, :].numpy(), out[idx, 1, :, :].numpy()
 
