5cb7d471bb7b3b6dd6f289569210a7684988719c
Ipsedo <berriens@gmail.com>
2021-06-21 20:15:25 +0200

    test AdaIN -> pas trop mal

--- networks/test_networks.py
+++ networks/test_networks.py
@@ -4,16 +4,18 @@ from networks import Generator, Discriminator
 if __name__ == '__main__':
     batch_size = 5
     rand_channels = 64
+    style_channels = 256
 
-    gen = Generator(rand_channels)
+    gen = Generator(rand_channels, style_channels)
     disc = Discriminator(2)
 
     print(gen)
     print(disc)
 
     rand = th.randn(5, rand_channels, 2, 2)
+    style_rand = th.randn(5, style_channels)
 
-    out = gen(rand)
+    out = gen(rand, style_rand)
 
     print(out.size())
 
