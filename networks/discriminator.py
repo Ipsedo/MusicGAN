test start from 4*4 + fix style computation

networks/discriminator.py
@@ -44,11 +44,11 @@ class MagPhaseLayer(nn.Sequential):
 class Discriminator(nn.Module):
     def __init__(
             self,
-            start_layer: int = 7
+            start_layer: int = 6
     ):
         super(Discriminator, self).__init__()
 
-        assert 0 <= start_layer <= 7
+        assert 0 <= start_layer <= 6
 
         conv_channels = [
             (32, 64),
@@ -58,8 +58,7 @@ class Discriminator(nn.Module):
             (112, 128),
             (128, 144),
             (144, 160),
-            (160, 176),
-            (176, 192)
+            (160, 176)
         ]
 
         self.__channels = conv_channels
@@ -68,7 +67,7 @@ class Discriminator(nn.Module):
 
         stride = 2
 
-        nb_layer = 9
+        nb_layer = 8
 
         self.__conv_blocks = nn.ModuleList([
             ConvBlock(

networks/generator.py
@@ -194,10 +194,9 @@ class Generator(nn.Module):
 
         self.__curr_layer = end_layer
 
-        self.__nb_downsample = 8
+        self.__nb_downsample = 7
 
         channels = [
-            (176, 160),
             (160, 144),
             (144, 128),
             (128, 112),
@@ -257,14 +256,16 @@ class Generator(nn.Module):
             alpha: float
     ) -> th.Tensor:
 
-        out = self.__first_block(z, z_style)
+        style = self.__style_network(z_style)
+
+        out = self.__first_block(z, style)
 
         for i in range(self.curr_layer):
             m = self.__gen_blocks[i]
-            out = m(out, z_style)
+            out = m(out, style)
 
         m = self.__gen_blocks[self.curr_layer]
-        out_blocks = m(out, z_style)
+        out_blocks = m(out, style)
 
         out_mp = self.__end_block(out_blocks)
 