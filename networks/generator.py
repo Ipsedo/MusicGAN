aa433f48f8cf79e0ae1c9cbda58fd66712df679d
Ipsedo <berriens@gmail.com>
2021-12-29 15:41:08 +0100

    increase channels in generator/discriminator

--- networks/generator.py
+++ networks/generator.py
@@ -50,14 +50,14 @@ class Generator(nn.Module):
         self.__nb_downsample = 7
 
         channels = [
-            (rand_channels, 64),
-            (64, 56),
-            (56, 48),
-            (48, 40),
-            (40, 32),
-            (32, 24),
-            (24, 16),
-            (16, 8)
+            (rand_channels, 256),
+            (256, 224),
+            (224, 192),
+            (192, 160),
+            (160, 128),
+            (128, 96),
+            (96, 64),
+            (64, 32)
         ]
 
         self.__channels = channels
