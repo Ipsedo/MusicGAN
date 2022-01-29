d865d35560764f5d3fca3bc950a0edfa39395238
Ipsedo <berriens@gmail.com>
2022-01-29 16:54:38 +0100

    test without style

--- networks/layers.py
+++ networks/layers.py
@@ -1,6 +1,5 @@
 import torch as th
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class PixelNorm(nn.Module):
@@ -22,41 +21,3 @@ class PixelNorm(nn.Module):
 
     def __str__(self):
         return self.__repr__()
-
-
-class AdaIN(nn.Module):
-    def __init__(
-            self,
-            channels: int,
-            style_channels: int
-    ):
-        super(AdaIN, self).__init__()
-
-        self.__to_style = nn.Linear(
-            style_channels,
-            2 * channels
-        )
-
-        self.__inst_norm = nn.InstanceNorm2d(
-            channels, affine=False
-        )
-
-        self.__channels = channels
-        self.__style_channels = style_channels
-
-    def forward(self, x: th.Tensor, z: th.Tensor) -> th.Tensor:
-        b, c, _, _ = x.size()
-
-        out_lin = self.__to_style(z).view(b, 2 * c, 1, 1)
-        gamma, beta = out_lin.chunk(2, 1)
-
-        out_norm = self.__inst_norm(x)
-
-        out = gamma * out_norm + beta
-
-        return out
-
-    def __repr__(self):
-        return "AdaIN" + \
-               f"(channels={self.__channels}, " \
-               f"style={self.__style_channels})"
