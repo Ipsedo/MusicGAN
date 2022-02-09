54eb987ab9a36ee5e59652176deb4c91a692d43a
Ipsedo <berriens@gmail.com>
2022-02-09 21:49:20 +0100

    fix tensor size() with batch dim....

--- music_gan/audio/transforms.py
+++ music_gan/audio/transforms.py
@@ -2,29 +2,33 @@ import torch as th
 
 
 class ChannelMinMaxNorm:
+    def __init__(self, epsilon=1e-8):
+        self.__epsilon = epsilon
+
     def __call__(self, x: th.Tensor) -> th.Tensor:
         assert len(x.size()) == 4
         assert x.size()[1] == 2
 
+        batch_size = x.size()[0]
+
         tmp_x = (
             x
-            .permute(1, 0, 2, 3)
-            .reshape(2, -1)
+            .view(batch_size, 2, -1)
         )
 
         x_max = (
             tmp_x
             .max(dim=-1)[0]
-            .view(1, 2, 1, 1)
+            .view(batch_size, 2, 1, 1)
         )
 
         x_min = (
             tmp_x
             .min(dim=-1)[0]
-            .view(1, 2, 1, 1)
+            .view(batch_size, 2, 1, 1)
         )
 
-        return (x - x_min) / (x_max - x_min)
+        return (x - x_min) / (x_max - x_min + self.__epsilon)
 
 
 class ChangeRange:
