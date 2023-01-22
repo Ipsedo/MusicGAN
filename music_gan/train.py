3c175c74f872bdff6f962184d5f224c5414bbeee
samuel <berriens@gmail.com>
2023-01-22 19:35:26 +0100

    removed nested mlfow run

--- music_gan/train.py
+++ music_gan/train.py
@@ -131,31 +131,80 @@ def train(
         }
     )
 
-    with mlflow.start_run(run_name="train", nested=True):
+    metric_window = 20
 
-        metric_window = 20
-        error_tp = [0.0 for _ in range(metric_window)]
-        error_tn = [0.0 for _ in range(metric_window)]
-        error_gen = [0.0 for _ in range(metric_window)]
+    error_tp = [0.0 for _ in range(metric_window)]
+    error_tn = [0.0 for _ in range(metric_window)]
+    error_gen = [0.0 for _ in range(metric_window)]
 
-        disc_error_list = [0.0 for _ in range(metric_window)]
-        disc_gp_list = [0.0 for _ in range(metric_window)]
+    disc_error_list = [0.0 for _ in range(metric_window)]
+    disc_gp_list = [0.0 for _ in range(metric_window)]
 
-        gen_error_list = [0.0 for _ in range(metric_window)]
+    gen_error_list = [0.0 for _ in range(metric_window)]
 
-        iter_idx = 0
+    iter_idx = 0
 
-        for e in range(nb_epoch):
+    for e in range(nb_epoch):
 
-            tqdm_bar = tqdm(data_loader)
+        tqdm_bar = tqdm(data_loader)
 
-            for x_real in tqdm_bar:
-                # [1] train discriminator
+        for x_real in tqdm_bar:
+            # [1] train discriminator
 
-                # pass data to cuda
-                x_real = x_real.to(th.float)
-                x_real = grower.scale_transform(x_real).cuda()
+            # pass data to cuda
+            x_real = x_real.to(th.float)
+            x_real = grower.scale_transform(x_real).cuda()
 
+            # sample random latent data
+            z = th.randn(
+                batch_size,
+                rand_channels,
+                height,
+                width,
+                device="cuda",
+            )
+
+            # gen fake data
+            x_fake = gen(z, grower.alpha)
+
+            # pass real data and gen data to discriminator
+            out_real = disc(x_real, grower.alpha)
+            out_fake = disc(x_fake, grower.alpha)
+
+            # compute discriminator loss
+            disc_error = networks.wasserstein_discriminator_loss(
+                out_real, out_fake
+            )
+
+            # compute gradient penalty
+            disc_gp = disc.gradient_penalty(
+                x_real,
+                x_fake,
+                grower.alpha,
+            )
+
+            disc_loss = disc_error + disc_gp
+
+            # reset grad
+            optim_disc.zero_grad(set_to_none=True)
+
+            # backward and optim step
+            disc_loss.backward()
+            optim_disc.step()
+
+            # discriminator metrics
+            del error_tn[0]
+            del error_tp[0]
+            error_tp.append(out_real.mean().item())
+            error_tn.append(out_fake.mean().item())
+
+            del disc_error_list[0]
+            del disc_gp_list[0]
+            disc_error_list.append(disc_error.item())
+            disc_gp_list.append(disc_gp.item())
+
+            # [2] train generator
+            if iter_idx % train_gen_every == 0:
                 # sample random latent data
                 z = th.randn(
                     batch_size,
@@ -165,133 +214,80 @@ def train(
                     device="cuda",
                 )
 
-                # gen fake data
+                # reset gradient
+                optim_disc.zero_grad(set_to_none=True)
+                optim_gen.zero_grad(set_to_none=True)
+
+                # generate fake data
                 x_fake = gen(z, grower.alpha)
 
-                # pass real data and gen data to discriminator
-                out_real = disc(x_real, grower.alpha)
+                # use unrolled discriminators
                 out_fake = disc(x_fake, grower.alpha)
 
-                # compute discriminator loss
-                disc_error = networks.wasserstein_discriminator_loss(
-                    out_real, out_fake
+                # compute generator error
+                gen_loss = networks.wasserstein_generator_loss(out_fake)
+
+                # reset gradient
+                optim_gen.zero_grad(set_to_none=True)
+
+                # backward pass and weight update
+                gen_loss.backward()
+                optim_gen.step()
+
+                # generator metrics
+                del error_gen[0]
+                error_gen.append(out_fake.mean().item())
+
+                del gen_error_list[0]
+                gen_error_list.append(gen_loss.item())
+
+            # update tqdm bar
+            tqdm_bar.set_description(
+                f"Epoch {e:02} "
+                f"[{saver.curr_save:03}: "
+                f"{saver.save_counter:04}], "
+                f"disc_l = {mean(disc_error_list):.4f}, "
+                f"gen_l = {mean(gen_error_list):.3f}, "
+                f"disc_gp = {mean(disc_gp_list):.3f}, "
+                f"e_tp = {mean(error_tp):.2f}, "
+                f"e_tn = {mean(error_tn):.2f}, "
+                f"e_gen = {mean(error_gen):.2f}, "
+                f"alpha = {grower.alpha:.3f} "
+            )
+
+            # log metrics
+            if iter_idx % 500 == 0:
+                mlflow.log_metrics(
+                    step=gen.curr_layer,
+                    metrics={
+                        "disc_loss": mean(disc_error_list),
+                        "gen_loss": mean(gen_error_list),
+                        "disc_gp": mean(disc_gp_list),
+                        "batch_tp_error": mean(error_tp),
+                        "batch_tn_error": mean(error_tn),
+                    },
                 )
 
-                # compute gradient penalty
-                disc_gp = disc.gradient_penalty(
-                    x_real,
-                    x_fake,
-                    grower.alpha,
+            # request save model
+            # each N forward/backward pass
+            saver.request_save(
+                gen,
+                disc,
+                optim_gen,
+                optim_disc,
+                grower.alpha,
+            )
+
+            iter_idx += 1
+
+            # ProGAN : add next layer
+            # IF time_to_grow AND growing
+            if grower.grow() and gen.growing:
+                gen.next_layer()
+                disc.next_layer()
+
+                tqdm_bar.write(
+                    "\n"
+                    f"Next layer, {gen.curr_layer} / {gen.layer_nb}, "
+                    f"curr_save = {saver.curr_save}"
                 )
-
-                disc_loss = disc_error + disc_gp
-
-                # reset grad
-                optim_disc.zero_grad(set_to_none=True)
-
-                # backward and optim step
-                disc_loss.backward()
-                optim_disc.step()
-
-                # discriminator metrics
-                del error_tn[0]
-                del error_tp[0]
-                error_tp.append(out_real.mean().item())
-                error_tn.append(out_fake.mean().item())
-
-                del disc_error_list[0]
-                del disc_gp_list[0]
-                disc_error_list.append(disc_error.item())
-                disc_gp_list.append(disc_gp.item())
-
-                # [2] train generator
-                if iter_idx % train_gen_every == 0:
-                    # sample random latent data
-                    z = th.randn(
-                        batch_size,
-                        rand_channels,
-                        height,
-                        width,
-                        device="cuda",
-                    )
-
-                    # reset gradient
-                    optim_disc.zero_grad(set_to_none=True)
-                    optim_gen.zero_grad(set_to_none=True)
-
-                    # generate fake data
-                    x_fake = gen(z, grower.alpha)
-
-                    # use unrolled discriminators
-                    out_fake = disc(x_fake, grower.alpha)
-
-                    # compute generator error
-                    gen_error = networks.wasserstein_generator_loss(out_fake)
-
-                    # generator loss
-                    gen_loss = gen_error
-
-                    # reset gradient
-                    optim_gen.zero_grad(set_to_none=True)
-
-                    # backward pass and weight update
-                    gen_loss.backward()
-                    optim_gen.step()
-
-                    # generator metrics
-                    del error_gen[0]
-                    error_gen.append(out_fake.mean().item())
-
-                    del gen_error_list[0]
-                    gen_error_list.append(gen_error.item())
-
-                # update tqdm bar
-                tqdm_bar.set_description(
-                    f"Epoch {e:02} "
-                    f"[{saver.curr_save:03}: "
-                    f"{saver.save_counter:04}], "
-                    f"disc_l = {mean(disc_error_list):.4f}, "
-                    f"gen_l = {mean(gen_error_list):.3f}, "
-                    f"disc_gp = {mean(disc_gp_list):.3f}, "
-                    f"e_tp = {mean(error_tp):.2f}, "
-                    f"e_tn = {mean(error_tn):.2f}, "
-                    f"e_gen = {mean(error_gen):.2f}, "
-                    f"alpha = {grower.alpha:.3f} "
-                )
-
-                # log metrics
-                if iter_idx % 500 == 0:
-                    mlflow.log_metrics(
-                        step=gen.curr_layer,
-                        metrics={
-                            "disc_loss": disc_loss.item(),
-                            "gen_loss": gen_loss.item(),
-                            "disc_gp": disc_gp.item(),
-                            "batch_tp_error": error_tp[-1],
-                            "batch_tn_error": error_tn[-1],
-                        },
-                    )
-
-                # request save model
-                # each N forward/backward pass
-                saver.request_save(
-                    gen,
-                    disc,
-                    optim_gen,
-                    optim_disc,
-                    grower.alpha,
-                )
-
-                iter_idx += 1
-
-                # ProGAN : add next layer
-                # IF time_to_grow AND growing
-                if grower.grow() and gen.growing:
-                    gen.next_layer()
-                    disc.next_layer()
-
-                    tqdm_bar.write(
-                        "\n"
-                        f"Next layer, {gen.curr_layer} / {gen.layer_nb}, "
-                        f"curr_save = {saver.curr_save}"
-                    )
